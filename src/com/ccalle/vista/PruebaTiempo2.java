335929fe73aec2c5bd08eb4e521ea0ed258192b2
Casa <Casa@Casa>
2018-11-19 23:02:37 -0500

    Ejercicio 8.3

---
+++ src/com/ccalle/vista/PruebaTiempo2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ccalle.vista;
+
+import com.ccalle.modelo.Tiempo1;
+
+/**
+ *
+ * @author Cristian Calle
+ */
+public class PruebaTiempo2 {
+    public static void main(String[] args) {
+        Tiempo1 hora = new Tiempo1();
+    }
+
+    
+}
